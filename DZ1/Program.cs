89b32df6edcf4cb7c5f9c8b64fe430e764cb2d67
DenisUstinovMIET <denisustinovmiet@mail.ru>
2022-09-18 23:14:27 +0300

    Create Program.cs

---
+++ DZ1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Дано значение температуры T в градусах Цельсия. 
+ * Определить значение этой же температуры в градусах Фаренгейта.
+ * Температура по Цельсию TC и температура 
+ * по Фаренгейту TF связаны следующим соотношением: TC = (TF – 32)·5/9.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите температуру в градусах Цельсия: ");
+			double TC = double.Parse(Console.ReadLine());
+			double TF = 9*TC/5+32;
+			Console.WriteLine($"Данная температуру в градусах Фаренгейта: {TF:0.000}");
+			Console.ReadLine();
+		}
+	}
+}
